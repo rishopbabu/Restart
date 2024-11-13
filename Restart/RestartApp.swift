3109e968e553d19ef4882089fb29f5ae1a196d3c
Rishop Babu <rishopbabu22@gmail.com>
2024-11-13 21:34:05 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Rishop Babu on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
